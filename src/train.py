11757dc8fb59f6cd75596d0381aaa6c201a851e3
nmstoker <git@nmstoker.com>
2024-01-24 00:27:17 +0000

    First commit

---
+++ src/train.py
@@ -0,0 +1,133 @@
+import torch
+from data import *
+from model import *
+import random
+import time
+import math
+
+n_hidden = 196
+n_epochs = 550000
+print_every = 10000
+plot_every = 1000
+learning_rate = 0.0033 # If you set this too high, it might explode. If too low, it might not learn
+
+def categoryFromOutput(output):
+    top_n, top_i = output.data.topk(1) # Tensor out of Variable with .data
+    category_i = top_i[0][0]
+    return all_categories[category_i], category_i
+
+def randomChoice(l):
+    return l[random.randint(0, len(l) - 1)]
+
+def randomTrainingPair():
+    category = randomChoice(all_categories)
+    line = randomChoice(category_lines[category])
+    category_tensor = Variable(torch.LongTensor([all_categories.index(category)]))
+    line_tensor = Variable(lineToTensor(line))
+    return category, line, category_tensor, line_tensor
+
+rnn = RNN(n_letters, n_hidden, n_categories)
+optimizer = torch.optim.SGD(rnn.parameters(), lr=learning_rate)
+criterion = nn.NLLLoss()
+
+def train(category_tensor, line_tensor):
+    hidden = rnn.initHidden()
+    optimizer.zero_grad()
+
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor[i], hidden)
+
+    loss = criterion(output, category_tensor)
+    loss.backward()
+
+    optimizer.step()
+
+    #return output, loss.data[0]
+    return output, loss.item()
+
+# Keep track of losses for plotting
+current_loss = 0
+all_losses = []
+
+def timeSince(since):
+    now = time.time()
+    s = now - since
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+start = time.time()
+
+for epoch in range(1, n_epochs + 1):
+    category, line, category_tensor, line_tensor = randomTrainingPair()
+    output, loss = train(category_tensor, line_tensor)
+    current_loss += loss
+
+    # Print epoch number, loss, name and guess
+    if epoch % print_every == 0:
+        guess, guess_i = categoryFromOutput(output)
+        correct = '✓' if guess == category else '✗ (%s)' % category
+        print('%d %d%% (%s) %.4f %s / %s %s' % (epoch, epoch / n_epochs * 100, timeSince(start), loss, line, guess, correct))
+
+    # Add current loss avg to list of losses
+    if epoch % plot_every == 0:
+        all_losses.append(current_loss / plot_every)
+        current_loss = 0
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import datetime
+
+output_date = datetime.datetime.now().strftime("%Y%m%dT%H%M%S")
+
+plt.figure()
+plt.plot(all_losses)
+plt.savefig(f'char-rnn-classification-losses-{output_date}.png', bbox_inches='tight')
+
+#------------------
+
+# Keep track of correct guesses in a confusion matrix
+confusion = torch.zeros(n_categories, n_categories)
+n_confusion = 10000
+
+# Just return an output given a line
+def evaluate(line_tensor):
+    hidden = rnn.initHidden()
+
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor[i], hidden)
+
+    return output
+
+# Go through a bunch of examples and record which are correctly guessed
+for i in range(n_confusion):
+    category, line, category_tensor, line_tensor = randomTrainingPair()
+    output = evaluate(line_tensor)
+    guess, guess_i = categoryFromOutput(output)
+    category_i = all_categories.index(category)
+    confusion[category_i][guess_i] += 1
+
+# Normalize by dividing every row by its sum
+for i in range(n_categories):
+    confusion[i] = confusion[i] / confusion[i].sum()
+
+# Set up plot
+fig = plt.figure()
+ax = fig.add_subplot(111)
+cax = ax.matshow(confusion.numpy())
+fig.colorbar(cax)
+
+# Set up axes
+ax.set_xticklabels([''] + all_categories, rotation=90)
+ax.set_yticklabels([''] + all_categories)
+
+# Force label at every tick
+ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+# sphinx_gallery_thumbnail_number = 2
+#plt.show()
+plt.savefig(f'char-rnn-classification-conf-matrix-{output_date}.png', bbox_inches='tight')
+
+torch.save(rnn, f'char-rnn-classification-{output_date}.pt')
+
